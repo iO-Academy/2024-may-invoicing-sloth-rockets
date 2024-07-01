21fd21b647e742aa10a47c47d2394d2ee99c4fbe
Will Atkinson <willreubena@gmail.com>
2024-07-01 15:12:24 +0100

    Started invoice container

---
+++ src/component/Invoice/index.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react"
+
+function Invoice() {
+    
+    
+    
+
+    return (
+        <>
+            <div className="p-10">
+                
+                <div>
+                    <p>Inovice Number</p>
+                    <p>Total</p>
+                </div>
+                
+                <p>Due Date</p>
+                
+                <div>
+                    <p>Name</p>
+                    <p>Status</p>
+                </div>
+                
+
+
+            </div>
+        </>
+    )
+
+}
+
+export default Invoice
--- src/component/InvoiceContainer/index.jsx
+++ src/component/InvoiceContainer/index.jsx
@@ -1,7 +1,26 @@
+import Invoice from "../Invoice"
+
 function InvoiceContainer(){
+    
+    useEffect(() => {
+		// Instructions here on what you need to happen on component mount
+		fetch('https://invoicing-api.dev.io-academy.uk/invoices')
+            .then(res => res.json())
+            .then((invoiceData) => {
+                console.log(invoiceData.data[0].invoice_id)
+
+            })
+    }, [])
+    
     return(
         <>
-            <h1>Goodbye World</h1>
+
+            <Invoice />
+            <Invoice />
+            <Invoice />
+            <Invoice />
+            <Invoice />
+
         </>
     )
 }
