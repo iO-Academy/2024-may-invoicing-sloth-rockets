e506fe81fad02c4a43e97141a34080611d726e90
Milan Fernando <milanburny@gmail.com>
2024-07-02 10:39:05 +0100

    Finished ViewHeader

--- src/component/ViewHeader/index.jsx
+++ src/component/ViewHeader/index.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom"
+
 function ViewHeader({id}) {
     return(
-        <div className="w-full p-3">
-            <span className="font-medium text-lg">Invoice #{id}</span>
-            <span className="float-right">X</span>
+        <div className="w-full p-3 border-b">
+            <span className="font-medium text-lg">Invoice <span className="text-gray-500">#</span>{id}</span>
+            <Link to={"/"} className="float-right text-gray-500">X </Link>
         </div>
     )
 }
