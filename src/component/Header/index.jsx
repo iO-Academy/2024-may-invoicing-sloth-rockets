357557e9d1b17f78d3b995d92294f9593873215c
Milan Fernando <milanburny@gmail.com>
2024-07-02 09:22:56 +0100

    Margins on everything

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -15,7 +15,7 @@ function Header() {
     }, [])
 
     return(
-        <header className="ml-[10%] pt-8 ">
+        <header className="pt-8 ">
             <h1 className="font-bold text-4xl pb-1">Invoices</h1>
             <p className="text-gray-500 pb-8 text-lg font-medium">There are {unpaid} unpaid invoices</p>
         </header>
