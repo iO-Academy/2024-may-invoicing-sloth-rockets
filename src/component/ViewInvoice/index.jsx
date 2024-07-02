a116b3056952f447065e52c5ca8500e43f1bd815
Milan Fernando <milanburny@gmail.com>
2024-07-02 16:31:16 +0100

    Finished Hopefully waiting on s2t2 to have status

--- src/component/ViewInvoice/index.jsx
+++ src/component/ViewInvoice/index.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom"
 import ViewHeader from "../ViewHeader"
 import { useEffect, useState } from "react"
 import moment from "moment"
+import Bill from "../Bill"
 
 function ViewInvoice({}){
     const {idName, id} = useParams()
@@ -11,9 +12,7 @@ function ViewInvoice({}){
         fetch(`https://invoicing-api.dev.io-academy.uk/invoices/${id}`)
             .then(res => res.json())
             .then((invoiceData) => {
-                console.log(invoiceData.data)
                 let data = invoiceData.data
-                console.log(invoiceData.data)
                 setClientInvoice({name:data.name
                     ,streetAddress:data.street_address
                     ,city:data.city
@@ -37,6 +36,7 @@ function ViewInvoice({}){
         <ViewHeader idName={idName} />
         <div className="grid grid-cols-2 place-content-around p-3">
             <div className="pl-1">
+
                 <p className="font-medium pb-2 pt-2">From</p>
                 <p>Kermit the Frog</p>
                 <p>Beverly Hills</p>
@@ -46,63 +46,56 @@ function ViewInvoice({}){
                 <p></p>
                 <p>{streetAddress}</p>
                 <p>{city}</p>
+
             </div>
             <div className="text-left justify-self-center">
-                <p className="font-medium pb-2 pt-2">Status</p>
 
+                <p className="font-medium pb-2 pt-2">Status</p>
                 <p className="font-medium pb-2">Created</p>
                 <p>{formattedDateCreated}</p>
                 <p className="font-medium pb-2 pt-4">Due</p>
                 <p>{formattedDateDue}</p>
+
             </div>
         </div>
+        <div className="py-4 p pl-5 pr-0 grid grid-cols-5 border-t border-b font-bold md:grid-cols-6 gap-1" >
 
-        <div className="grid grid-cols-4 border-t border-b p-4 font-bold pl-5" >
-
-
-            <p>Description</p>
+            <p className="col-span-2 md:col-span-3">Description</p>
             <p>Quantity</p>
             <p>Rate</p>
             <p>Total</p>
 
         </div>
+        {details.map(detail=>{
+            return(
+                <Bill key={detail.description} detail={detail}/>
+            )
+        })}
+        <div className=" p-4 pr-0 grid grid-cols-5 md:grid-cols-6 border-t border-b">
 
-        <div className="pl-5">
-
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-            <p>Content</p>
-
-        </div>
-
-        <div className="grid grid-cols-3 border-t border-b p-4">
+            <p className="col-span-2 md:col-span-3"></p>
+            <p className="text-right">Total</p>
             <p></p>
-            <p>Total</p>
             <p className="font-bold">£{invoiceTotal}</p>
 
-
         </div>
+        <div className=" p-4 pr-0 grid  grid-cols-5 md:grid-cols-6 border-t border-b ">
 
-        <div className="grid grid-cols-3 border-t border-b p-4">
+            <p className="col-span-2 md:col-span-3" ></p>
+            <p className="text-right">Paid to date</p>
             <p></p>
-            <p>Paid to date</p>
             <p className="font-bold">£{paidToDate}</p>
 
+        </div>
+        <div className="bg-[#FEC007] p-4 pr-0 grid grid-cols-5 md:grid-cols-6 border-t border-b font-bold">
+
+            <p className="col-span-2 md:col-span-3"></p>
+            <p className="text-right">Total due</p>
+            <p> </p>
+            <p>£{parseFloat(invoiceTotal) + parseFloat(paidToDate)}</p>
 
         </div>
-        <div className="grid grid-cols-3 border-t border-b p-4 bg-[#FEC007] font-bold">
-            <p></p>
-            <p>Total due</p>
-            <p>£{invoiceTotal + paidToDate}</p>
-
-
-        </div>
-
         <p className="border-b pt-4 pb-8">Payments due within 30 days.</p>
-        
-
-        
         </>
     )
 }
