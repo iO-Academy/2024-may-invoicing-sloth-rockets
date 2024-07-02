b39c29617feb4444481ac21f84c4abfe18094ffc
Maryam Wardak <mmwk183@gmail.com>
2024-07-02 14:41:51 +0100

    added styling to view invoice

--- src/component/ViewInvoice/index.jsx
+++ src/component/ViewInvoice/index.jsx
@@ -36,7 +36,7 @@ function ViewInvoice({}){
         <>
         <ViewHeader idName={idName} />
         <div className="grid grid-cols-2 place-content-around p-3">
-            <div className="pl-3">
+            <div className="pl-1">
                 <p className="font-medium pb-2 pt-2">From</p>
                 <p>Kermit the Frog</p>
                 <p>Beverly Hills</p>
@@ -56,6 +56,53 @@ function ViewInvoice({}){
                 <p>{formattedDateDue}</p>
             </div>
         </div>
+
+        <div className="grid grid-cols-4 border-t border-b p-4 font-bold pl-5" >
+
+
+            <p>Description</p>
+            <p>Quantity</p>
+            <p>Rate</p>
+            <p>Total</p>
+
+        </div>
+
+        <div className="pl-5">
+
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+            <p>Content</p>
+
+        </div>
+
+        <div className="grid grid-cols-3 border-t border-b p-4">
+            <p></p>
+            <p>Total</p>
+            <p className="font-bold">£{invoiceTotal}</p>
+
+
+        </div>
+
+        <div className="grid grid-cols-3 border-t border-b p-4">
+            <p></p>
+            <p>Paid to date</p>
+            <p className="font-bold">£{paidToDate}</p>
+
+
+        </div>
+        <div className="grid grid-cols-3 border-t border-b p-4 bg-[#FEC007] font-bold">
+            <p></p>
+            <p>Total due</p>
+            <p>£{invoiceTotal + paidToDate}</p>
+
+
+        </div>
+
+        <p className="border-b pt-4 pb-8">Payments due within 30 days.</p>
+        
+
+        
         </>
     )
 }
