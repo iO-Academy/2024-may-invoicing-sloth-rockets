693a521632020c32b39785fc9d58cbff31d76c00
Milan Fernando <milanburny@gmail.com>
2024-07-02 09:07:24 +0100

    Finished

--- src/component/InvoiceContainer/index.jsx
+++ src/component/InvoiceContainer/index.jsx
@@ -2,35 +2,20 @@ import { useEffect, useState } from "react"
 import Invoice from "../Invoice"
 
 function InvoiceContainer(){
-
-    const [invoiceTotal, setInvoiceTotal] = useState(0)
-    const [invoiceStatus, setInvoiceStatus] = useState(0)
-    const [clientName, setClientName] = useState(0)
-    const [invoiceId, setInvoiceId] = useState(0)
-    const [dueDate, setdueDate] = useState(0)
-
+    const [invoices, setInvoices] = useState([])
     useEffect(() => {
 		// Instructions here on what you need to happen on component mount
 		fetch('https://invoicing-api.dev.io-academy.uk/invoices')
             .then(res => res.json())
             .then((invoiceData) => {
-                setInvoiceTotal(invoiceData.data[0].invoice_total)
-                setInvoiceStatus(invoiceData.data[0].status_name)
-                setClientName(invoiceData.data[0].name)
-                setInvoiceId(invoiceData.data[0].invoice_id)
-                setdueDate(invoiceData.data[0].due)
+                setInvoices(invoiceData.data)
 
             })
     }, [])
     
     return(
         <>
-
-            <Invoice invoiceTotal={invoiceTotal} invoiceStatus={invoiceStatus} clientName={clientName} invoiceId={invoiceId} dueDate={dueDate}/>
-            <Invoice invoiceTotal={invoiceTotal} invoiceStatus={invoiceStatus} clientName={clientName} invoiceId={invoiceId} dueDate={dueDate}/>
-            <Invoice invoiceTotal={invoiceTotal} invoiceStatus={invoiceStatus} clientName={clientName} invoiceId={invoiceId} dueDate={dueDate}/>
-            <Invoice invoiceTotal={invoiceTotal} invoiceStatus={invoiceStatus} clientName={clientName} invoiceId={invoiceId} dueDate={dueDate}/>
-            <Invoice invoiceTotal={invoiceTotal} invoiceStatus={invoiceStatus} clientName={clientName} invoiceId={invoiceId} dueDate={dueDate}/>
+            {invoices.map(invoice => <Invoice invoiceTotal={invoice.invoice_total} invoiceStatus={invoice.status_name} clientName={invoice.name} invoiceId={invoice.invoice_id} dueDate={invoice.due}/>)}
 
         </>
     )
