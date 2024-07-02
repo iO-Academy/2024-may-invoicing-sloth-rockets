8a7ac26b4cf22a0c7724fbf2367fbfde92f8ddfa
Will Atkinson <willreubena@gmail.com>
2024-07-02 10:33:19 +0100

    started styling invoices

--- src/component/Invoice/index.jsx
+++ src/component/Invoice/index.jsx
@@ -8,18 +8,18 @@ function Invoice({invoiceTotal, invoiceStatus, clientName, invoiceId, dueDate}) 
 
     return (
         <>
-            <div className="p-10">
+            <div className="p-2 mb-4 border-2 border-black flex flex-row justify-between">
                 
-                <div>
-                    <p>{invoiceId}</p>
-                    <p>{invoiceTotal}</p>
+                <div className="p-5">
+                    <p className="font-bold text-base"><span className="text-gray-500">#</span>{invoiceId}</p>
+                    <p className="font-bold text-2xl">£{invoiceTotal}</p>
                 </div>
                 
-                <p>Due {formattedDate}</p>
+                <p className="p-5 text-gray-500 text-base font-medium" >Due {formattedDate}</p>
                 
-                <div>
-                    <p>{clientName}</p>
-                    <p>{invoiceStatus}</p>
+                <div className="p-5 flex flex-col" >
+                    <p className="text-gray-500 text-base font-medium">{clientName}</p>
+                    <p className="self-end" >{invoiceStatus}</p>
                 </div>
 
             </div>
