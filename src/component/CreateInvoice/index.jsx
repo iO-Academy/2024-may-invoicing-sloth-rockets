6916d362ee66c0c476d34d88214ec57f7fa48027
Vicky Greig <vickygreig@hotmail.co.uk>
2024-07-03 10:09:04 +0100

    start of fetch

--- src/component/CreateInvoice/index.jsx
+++ src/component/CreateInvoice/index.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react"
+
 function CreateInvoice(){
+
+    const [clients, setClients] = useState([])
+    const [clientName, setClientName] = useState('')
+    const [clientStreet, setClientStreet] = useState('')
+    const [clientCity, setClientCity] =useState('')
+
+    useEffect(()=>{
+        fetch('https://invoicing-api.dev.io-academy.uk/clients')
+            .then(res => res.json())
+            .then((clientsData) => {
+                console.log(clientsData.data)
+                setClients =(clientsData.data)
+                setClientName(data.name) 
+                setClientStreet(data.streetAddress)
+                setClientCity(data.city)
+             })
+    }, [])
+
     return(
         <>
+           {clients.map(client => <p>{client.name} {client.streetAddress} {client.city}</p>)}
 
         </>
     )
