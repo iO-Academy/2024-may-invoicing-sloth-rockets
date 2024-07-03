a3fcb1ab1fb98bdce0151b764815a28001a2db18
Will Atkinson <willreubena@gmail.com>
2024-07-03 10:44:40 +0100

    dropdown menu started

--- src/component/CreateInvoice/index.jsx
+++ src/component/CreateInvoice/index.jsx
@@ -5,24 +5,29 @@ function CreateInvoice(){
     const [clients, setClients] = useState([])
     const [clientName, setClientName] = useState('')
     const [clientStreet, setClientStreet] = useState('')
-    const [clientCity, setClientCity] =useState('')
+    const [clientCity, setClientCity] = useState('')
 
     useEffect(()=>{
         fetch('https://invoicing-api.dev.io-academy.uk/clients')
             .then(res => res.json())
             .then((clientsData) => {
                 console.log(clientsData.data)
-                setClients =(clientsData.data)
-                setClientName(data.name) 
-                setClientStreet(data.streetAddress)
-                setClientCity(data.city)
+                setClients(clientsData.data)
+                setClientName(clients.name) 
+                setClientStreet(clients.street_address)
+                setClientCity(clients.city)
+                
              })
     }, [])
 
-    return(
-        <>
-           {clients.map(client => <p>{client.name} {client.streetAddress} {client.city}</p>)}
+    // <p>{client.name}, {client.street_address}, {client.city}</p>)}
 
+    return(
+        <>  
+            <select>
+                {clients.map(client => <option value="hi">hello</option>)}
+            </select>
+    
         </>
     )
 }
