8ec926f7c30d4201c95aa6f84fc8efd8041f8fa3
Milan Fernando <milanburny@gmail.com>
2024-07-03 09:10:32 +0100

    Final Version 2.0

--- src/component/StatusIcon/index.jsx
+++ src/component/StatusIcon/index.jsx
@@ -4,26 +4,21 @@ function StatusIcon({ invoiceStatus }) {
     useEffect(iconColor, [])
 
     const [statusColor, setStatusColor] = useState('')
-    const [statusBorderColor, setStatusBorderColor] = useState('')
 
     function iconColor() {
         if (invoiceStatus === "Paid") {
-            setStatusColor("text-green-500")
-            setStatusBorderColor("border-green-500")
+            setStatusColor("green-500")
         } else if (invoiceStatus === "Pending") {
-            setStatusColor("text-yellow-400")
-            setStatusBorderColor("border-yellow-400")
+            setStatusColor("yellow-400")
         } else if (invoiceStatus === "Overdue") {
-            setStatusColor("text-red-600")
-            setStatusBorderColor("border-red-600")
+            setStatusColor("red-600")
         } else {
-            setStatusColor("text-zinc-500")
-            setStatusBorderColor("border-zinc-500")
+            setStatusColor("zinc-500")
         }
     }
 
     return (
-        <p className={`${statusColor} ${statusBorderColor} border-2 p-2 rounded-md`} ><span className="text-xl">•</span>{invoiceStatus}</p>
+        <p className={`text-${statusColor} border-${statusColor} border-2 p-2 rounded-md`} ><span className="text-xl">•</span>{invoiceStatus}</p>
     )
 }
 
